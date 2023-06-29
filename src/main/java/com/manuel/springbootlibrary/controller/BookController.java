4ef95a41906d5cec4cf1150a51144b09f8dd9d29
Tzuyu <manekete@gmail.com>
2023-06-29 16:22:46 -0600

    Add okta security and JWT

--- src/main/java/com/manuel/springbootlibrary/controller/BookController.java
+++ src/main/java/com/manuel/springbootlibrary/controller/BookController.java
@@ -2,6 +2,7 @@ package com.manuel.springbootlibrary.controller;
 
 import com.manuel.springbootlibrary.entity.Book;
 import com.manuel.springbootlibrary.service.BookService;
+import com.manuel.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,21 +18,23 @@ public class BookController {
 
     }
 
-    @GetMapping("/secure/currentloans/count")
-    public int currentLoansCount() {
-        String  userEmail = "testuser@email.com";
+    @GetMapping("/secure/currentloans/count" )
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String  userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoanCount(userEmail);
     }
 
     @GetMapping("/secure/ischeckedout/byuser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId){
-        String userEmail = "testuser@email.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token,
+                                      @RequestParam Long bookId){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @PutMapping("/secure/checkout")
-    public Book checkoutBook (@RequestParam Long bookId) throws Exception {
-        String userEmail = "testuser@email.com";
+    public Book checkoutBook (@RequestHeader(value = "Authorization") String token,
+                              @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkOutBook(userEmail, bookId);
 
     }
